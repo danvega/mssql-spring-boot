06c3fc1a773e77888fb7ef5e166e9f7147070b94
Dan Vega <danvega@gmail.com>
2020-09-11 16:48:51 -0400

    smoke test

--- src/test/java/dev/danvega/mssqldemo/MssqlDemoApplicationTests.java
+++ src/test/java/dev/danvega/mssqldemo/MssqlDemoApplicationTests.java
@@ -1,13 +1,21 @@
 package dev.danvega.mssqldemo;
 
+import dev.danvega.mssqldemo.controller.CourseController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MssqlDemoApplicationTests {
 
+    @Autowired
+    CourseController courseController;
+
     @Test
     void contextLoads() {
+        // sanity test
+        Assertions.assertNotNull(courseController);
     }
 
 }
